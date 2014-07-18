772652332d0b3350dda3e26419b8467081f285d9
Sergii Boiko <cris.kiev@gmail.com>
2014-07-17 22:23:14 -0700

    update lanterna to 2.1.8

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clojure-lanterna "0.9.4"
+(defproject clojure-lanterna "0.9.5"
   :description "A Clojure wrapper around the Lanterna terminal output library."
   :url "http://sjl.bitbucket.org/clojure-lanterna/"
   :license {:name "LGPL"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.googlecode.lanterna/lanterna "2.1.5"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.googlecode.lanterna/lanterna "2.1.8"]]
   :java-source-paths ["./java"]
   ; :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
   )
